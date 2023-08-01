7c1ca65126972d172e37633fa7c143cc5c820760
bestfriendai <120160378+bestfriendai@users.noreply.github.com>
2023-08-01 11:52:59 -0400

    Update nav.js

--- components/nav.js
+++ components/nav.js
@@ -120,14 +120,7 @@ export default function Nav() {
                     )}
                   </button>
                 )}
-                <Link
-                  href="/memories"
-                  type="button"
-                  className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  <PhotoIcon className="h-5 w-5 text-gray-400 mr-2 hidden sm: inline-flex" />
-                  Memories
-                </Link>
+              
               </div>
             )}
           </div>
